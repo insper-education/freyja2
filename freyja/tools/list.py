0d790b05f019de64c91254dcdb1397fa545cdbd2
Rafael Corsi <corsiferrao@gmail.com>
2023-02-28 08:43:13 -0600

    fix list update

--- freyja/tools/list.py
+++ freyja/tools/list.py
@@ -22,8 +22,8 @@ def get_repos(org_name, prefix, size=500):
     return(search(rawList, prefix))
 
 
-def append_url(repos, https):
-    if https is False:
+def append_url(repos, ssh):
+    if ssh is True:
         base_url = 'git@github.com:'
     else:
         base_url = 'https://'
@@ -42,13 +42,13 @@ def list():
     help="Gh organization name to search"
 )
 @click.option(
-    "--https",
+    "--ssh",
     default=False,
-    help="Create the list with https:// instead ssh://"
+    help="Create the list with ssh:// instead https://"
 )
 @list.command()
 @click.pass_obj
-def create(freyja, org_name, prefix, https):
+def create(freyja, org_name, prefix, ssh):
     if freyja.open_repos_file(log=False) is True:
         click.echo('Repos list already exist do you want to update it? run: freyja list update')
         return 0
@@ -56,25 +56,29 @@ def create(freyja, org_name, prefix, https):
     freyja.org = org_name
     freyja.prefix = prefix
     freyja.repos_name = get_repos(org_name, prefix)
-    freyja.repos_url = append_url(freyja.repos_name, https)
+    freyja.repos_url = append_url(freyja.repos_name, ssh)
     freyja.write_config()
 
 
 @list.command()
 @click.pass_obj
 def update(freyja):
+    update = False
     if freyja.open_repos_file() is False:
-        return false
+        return False
 
     c_repos_name = get_repos(freyja.org, freyja.prefix)
     c_repos_url = append_url(c_repos_name, False)
     cnt = 0
-    for r in c_repos_name:
+    for r in c_repos_url:
         if r not in freyja.repos_url:
             cnt = cnt + 1
             freyja.repos_url.append(r)
-    click.echo("{} updated with {} repos".format(freyja.repos_yml, cnt))
-    freyja.write_config()
+            update = True
+
+    click.echo("Found {} new repos with the prefix {}".format(cnt, freyja.prefix))
+    if update:
+        freyja.write_config()
 
 
 @list.command()
